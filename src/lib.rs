d925d00959caffd732772a357f352715841515e2
darcy <acednes@gmail.com>
2023-12-04 19:09:17 +1100

    test cache/no-cache

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,18 @@ const ATTEMPTS: u32 = 10;
 const TIMEOUT: u32 = 10;
 
 pub struct Everygarf {
-    pub max: u32,
+    pub images: u32,
     pub jobs: u32,
+    pub cache: bool,
 }
 
 impl Everygarf {
-    pub fn new(max: u32, jobs: u32) -> Self {
-        Self { max, jobs }
+    pub fn new(images: u32, jobs: u32, cache: bool) -> Self {
+        Self {
+            images,
+            jobs,
+            cache,
+        }
     }
 
     pub fn execute(self) -> Option<Duration> {
@@ -24,8 +29,11 @@ impl Everygarf {
         command.arg("--notify-fail");
         command.args(["--attempts", &ATTEMPTS.to_string()]);
         command.args(["--timeout", &TIMEOUT.to_string()]);
-        command.args(["--max", &self.max.to_string()]);
+        command.args(["--max", &self.images.to_string()]);
         command.args(["--jobs", &self.jobs.to_string()]);
+        if !self.cache {
+            command.arg("--no-cache");
+        }
 
         let start_time = Instant::now();
         let output = command.output().expect("Failed to run command");
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,27 @@ use std::path::Path;
 use everygarf_stats::Everygarf;
 
 fn main() {
-    let output_file = "everygarf.csv";
+    let dir = Path::new("stats");
+    if Path::new(dir).exists() {
+        fs::remove_dir_all(dir).expect("Failed to remove directory");
+    }
+    fs::create_dir(dir).expect("Failed to create directory");
+
+    run(&dir.join(&Path::new("no-cache.csv")), false);
+    run(&dir.join(&Path::new("cache.csv")), true);
+}
+
+fn run(filepath: &Path, cache: bool) {
     let images_values = [
         1, 5, 10, 20, 30, 50, 100, 200, 300, 500, 1_000, 5_000, 1_500,
     ];
     let jobs_min = 1;
     let jobs_max = 50;
 
-    let mut rows = Vec::new();
-
-    if Path::new(output_file).exists() {
-        fs::write(output_file, "").expect("Failed to truncate file");
-    }
     let mut file = fs::OpenOptions::new()
         .create(true)
         .append(true)
-        .open(output_file)
+        .open(filepath)
         .expect("Failed to open file");
 
     macro_rules! write_file {
@@ -37,11 +42,9 @@ fn main() {
 
     println!("\x1b[1mprogress    jobs   images      time\x1b[0m");
     for (i, jobs) in (jobs_min..=jobs_max).enumerate() {
-        let mut cells = Vec::new();
-
         write_file!("{:-7}", jobs);
         for (j, images) in images_values.iter().enumerate() {
-            let Some(time) = Everygarf::new(*images, jobs as u32).execute() else {
+            let Some(time) = Everygarf::new(*images, jobs as u32, cache).execute() else {
                 return;
             };
             let time = time.as_secs_f32();
@@ -52,10 +55,7 @@ fn main() {
             println!("{percent:-7.1}% {jobs:-7}  {images:-7}  {time:-7}s");
 
             write_file!(", {:-7}", time);
-            cells.push(time);
         }
         write_file!("\n");
-
-        rows.push(cells);
     }
 }
