d3182c900e75a656a49ab2b25019b950858d9a58
darcy <acednes@gmail.com>
2023-12-06 13:56:40 +1100

    make better!

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use std::fs;
 use std::io::Write;
 use std::path::Path;
 
+use chrono::Utc;
 use everygarf_stats::Everygarf;
 
 fn main() {
     let dir = Path::new("stats");
-    if Path::new(dir).exists() {
-        fs::remove_dir_all(dir).expect("Failed to remove directory");
+    let old_dir = Path::new("old");
+
+    if dir.exists() {
+        if !old_dir.exists() {
+            fs::create_dir_all(old_dir).expect("Failed to make old directory");
+        }
+        let date = Utc::now().format("%y%m%d-%H%M").to_string();
+        fs::rename(dir, old_dir.join(Path::new(&date))).expect("Failed to move old directory");
     }
     fs::create_dir(dir).expect("Failed to create directory");
 
@@ -16,11 +23,11 @@ fn main() {
 }
 
 fn run(filepath: &Path, cache: bool) {
-    let images_values = [
-        1, 5, 10, 20, 30, 50, 100, 200, 300, 500, 1_000, 5_000, 1_500,
+    let images_values = [1, 5, 10, 20, 30, 50, 100, 200, 300, 500, 1_000];
+    let jobs_values = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+        30, 35, 40, 45, 50,
     ];
-    let jobs_min = 1;
-    let jobs_max = 50;
 
     let mut file = fs::OpenOptions::new()
         .create(true)
@@ -41,20 +48,19 @@ fn run(filepath: &Path, cache: bool) {
     write_file!("\n");
 
     println!("\x1b[1mprogress    jobs   images      time\x1b[0m");
-    for (i, jobs) in (jobs_min..=jobs_max).enumerate() {
+    for (i, jobs) in jobs_values.iter().enumerate() {
         write_file!("{:-7}", jobs);
         for (j, images) in images_values.iter().enumerate() {
-            let Some(time) = Everygarf::new(*images, jobs as u32, cache).execute() else {
+            let Some(time) = Everygarf::new(*images, *jobs, cache).execute() else {
                 return;
             };
             let time = time.as_secs_f32();
-            let time = (time * 10.0).round() / 10.0;
 
             let percent = 100.0 * (i * images_values.len() + j) as f32
-                / (images_values.len() * (1 + jobs_max - jobs_min)) as f32;
+                / (images_values.len() * jobs_values.len()) as f32;
             println!("{percent:-7.1}% {jobs:-7}  {images:-7}  {time:-7}s");
 
-            write_file!(", {:-7}", time);
+            write_file!(", {:-7.2}", time);
         }
         write_file!("\n");
     }
